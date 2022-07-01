6ffc8a3ac9c8bbee83194d8b943b75234e15e208
Novaneel <novaneel2802@gmail.com>
2022-07-01 15:17:02 +0530

    updated

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const form = document.querySelector('#searchform');
+const form2 = document.querySelector('#reset');
+form2.addEventListener('click' , function(e){
+    // setTimeout(() => {
+    //     rmImages();
+    // }, 5000)
+    rmImages();
+})
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
     // console.log("SUBMITTED")
@@ -8,22 +15,45 @@ form.addEventListener('submit', async function (e) {
     const res = await axios.get(`https://api.tvmaze.com/search/shows`,config )
     console.log(res.data);
     console.log(res.data.length);
-
-    // const img = document.createElement('IMG');
-    // img.src = res.data[0].show.image.medium;
-    // document.body.append(img)
     makeImages(res.data);
     form.elements.query.value = '';
+
 })
 
 const makeImages = (shows) => {
+    // document.querySelector('#newdiv').createElement('div')
+    const newdiv = document.querySelector('#newdiv')
     for (let result of shows) {
         // console.log(show)
-        if (result.show.image) {
+        if (result.show.image && result.show.name) {
+            const div = document.createElement('div')
+            // div.classList.add('d-flex')
+            div.style.margin = "2px";
+            div.classList.add("mov-det")
             const img = document.createElement('IMG');
             img.src = result.show.image.medium;
-            document.body.append(img);
+            img.classList.add("rounded")
+
+            const name = document.createElement('h5')
+            name.innerText = result.show.name;
+            name.classList.add("mx-auto")
+            name.style.fontFamily = "'Gentium Book Plus', serif" ;
+            newdiv.append(div);
+            div.appendChild(img);
+            // newdiv.appendChild(img);
+            div.appendChild(name);
         }
 
     }
+}
+
+
+const rmImages = () => {
+    let rmim = document.querySelectorAll('.mov-det')
+    rmim.forEach(img => {
+        img.remove();
+    });
+    rmim.forEach(name => {
+        name.remove();
+    });
 }
